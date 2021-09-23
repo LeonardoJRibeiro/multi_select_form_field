c9ee9afc325fca2f44d91815c77f671b995da9fa
leonardojribeiro <leonardojr410@gmail.com>
2021-09-22 22:14:31 -0300

    create example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,113 +1,120 @@
 import 'package:flutter/material.dart';
 
+import 'package:multi_select_form_field/multi_select_form_field.dart';
+
 void main() {
-  runApp(MyApp());
+  runApp(Example());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class Example extends StatefulWidget {
+  const Example({Key? key}) : super(key: key);
   @override
+  _ExampleState createState() => _ExampleState();
+}
+
+class _ExampleState extends State<Example> {
+  final colorsController = MultiSelectController<ColorModel>();
+  final formKey = GlobalKey<FormState>();
+  final data = [
+    ColorModel(name: 'Red', color: Colors.red),
+    ColorModel(name: 'Blue', color: Colors.blue),
+    ColorModel(name: 'Green', color: Colors.green),
+    ColorModel(name: 'Yellow', color: Colors.yellow),
+    ColorModel(name: 'Grey', color: Colors.grey),
+    ColorModel(name: 'White', color: Colors.white),
+    ColorModel(name: 'Cyan', color: Colors.cyan),
+  ];
+
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Multi Select Form Field Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        body: SafeArea(
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                MultiSelectFormField<ColorModel>(
+                  title: 'Select colors',
+                  controller: colorsController,
+                  itemSelectedBuilder: (item, onDelete) => Chip(
+                    label: Text(item.name),
+                    onDeleted: onDelete,
+                    backgroundColor: item.color,
+                  ),
+                  itens: data,
+                  itemTileBuilder: (item, selected, onChange) {
+                    return Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: SwitchListTile(
+                        title: Text(item.name),
+                        tileColor: item.color,
+                        value: selected,
+                        onChanged: onChange,
+                      ),
+                    );
+                  },
+                  validator: (itens) {
+                    if (itens.isEmpty) {
+                      return 'At least one item is required';
+                    }
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ValueListenableBuilder<List<ColorModel>>(
+                    valueListenable: colorsController,
+                    builder: (context, value, child) => Text(''),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    formKey.currentState?.validate();
+                  },
+                  child: const Text('Validate'),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class ColorModel {
+  final String name;
+  final Color color;
+
+  ColorModel({
+    required this.name,
+    required this.color,
+  });
+
+  ColorModel copyWith({
+    String? name,
+    Color? color,
+  }) {
+    return ColorModel(
+      name: name ?? this.name,
+      color: color ?? this.color,
+    );
+  }
+
+  @override
+  String toString() => 'ColorModel(name: $name, color: $color)';
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is ColorModel && other.name == name && other.color == color;
+  }
+
+  @override
+  int get hashCode => name.hashCode ^ color.hashCode;
 }
